9a54c05ee2e92d9d7d4bac45417aa927826ce61e
Andy Fillebrown <andy@docEdub.com>
2024-08-27 16:30:54 -0400

    wip on sound instance ownership
    With this change, sound instances are owned by the audio engine and connect downstream to the sound object that the instance is created for when `SoundObject.play()` is called.

--- packages/dev/core/src/Audio/v2/abstractAudioEngine.ts
+++ packages/dev/core/src/Audio/v2/abstractAudioEngine.ts
@@ -7,14 +7,26 @@ import type { AbstractAudioNode } from "./abstractAudioNode";
 import type { AbstractAudioPositioner } from "./abstractAudioPositioner";
 import type { AbstractAudioSender } from "./abstractAudioSender";
 import type { AbstractMainAudioBus } from "./abstractMainAudioBus";
+import type { AbstractSoundSource } from "./abstractSoundSource";
 import type { AbstractStaticSoundInstance } from "./abstractStaticSoundInstance";
 import type { AbstractStaticSoundSource } from "./abstractStaticSoundSource";
 import type { AbstractStreamingSoundInstance } from "./abstractStreamingSoundInstance";
 import type { AbstractStreamingSoundSource } from "./abstractStreamingSoundSource";
 import type { IAudioNodeParent } from "./IAudioNodeParent";
 
+/**
+ * Owns top-level AbstractAudioNode objects.
+ * Owns all AbstractSoundSource objects.
+ */
 export abstract class AbstractAudioEngine implements IAudioNodeParent {
     public dispose(): void {
+        this._soundInstances.length = 0;
+
+        for (const source of this._soundSources) {
+            source.dispose();
+        }
+        this._soundSources.length = 0;
+
         for (const node of this._childNodes) {
             node.dispose();
         }
@@ -40,6 +52,8 @@ export abstract class AbstractAudioEngine implements IAudioNodeParent {
         this._childNodes.splice(index, 1);
     }
 
+    // NB: Does not indicate ownership, but all its items should be in the child nodes array, too, which does indicate
+    // ownership.
     private _soundInstances = new Array<AbstractStaticSoundInstance>();
 
     public _addSoundInstance(instance: AbstractStaticSoundInstance): void {
@@ -59,11 +73,30 @@ export abstract class AbstractAudioEngine implements IAudioNodeParent {
         this._soundInstances.splice(index, 1);
     }
 
+    private _soundSources = new Array<AbstractSoundSource>();
+
+    public _addSoundSource(soundSource: AbstractSoundSource): void {
+        if (this._soundSources.includes(soundSource)) {
+            return;
+        }
+
+        this._soundSources.push(soundSource);
+    }
+
+    public _removeSoundSource(soundSource: AbstractSoundSource): void {
+        const index = this._soundSources.indexOf(soundSource);
+        if (index < 0) {
+            return;
+        }
+
+        this._soundSources.splice(index, 1);
+    }
+
     public abstract createDevice(name: string): AbstractAudioDevice;
     public abstract createListener(parent: AbstractAudioDevice): AbstractAudioListener;
     public abstract createMainBus(name: string): AbstractMainAudioBus;
     public abstract createPositioner(parent: AbstractAudioNode): AbstractAudioPositioner;
     public abstract createSender(parent: AbstractAudioNode): AbstractAudioSender;
-    public abstract createStaticSoundInstance(source: AbstractStaticSoundSource): AbstractStaticSoundInstance;
-    public abstract createStreamingSoundInstance(source: AbstractStreamingSoundSource): AbstractStreamingSoundInstance;
+    public abstract createStaticSoundInstance(source: AbstractStaticSoundSource, inputNode: AbstractAudioNode): AbstractStaticSoundInstance;
+    public abstract createStreamingSoundInstance(source: AbstractStreamingSoundSource, inputNode: AbstractAudioNode): AbstractStreamingSoundInstance;
 }
