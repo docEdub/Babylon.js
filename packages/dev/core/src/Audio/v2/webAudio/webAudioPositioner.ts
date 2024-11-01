e2b52d4a4ba77ce153e709a177a83a9c6f49af88
Andy Fillebrown <andy@docEdub.com>
2024-11-01 15:29:57 -0400

    Mark webAudioPositioner.ts exports as internal

--- packages/dev/core/src/Audio/v2/webAudio/webAudioPositioner.ts
+++ packages/dev/core/src/Audio/v2/webAudio/webAudioPositioner.ts
@@ -4,17 +4,10 @@ import type { AudioPositionerOptions } from "../audioPositioner";
 import { AudioPositioner } from "../audioPositioner";
 import type { SpatialAudioTransformOptions } from "../spatialAudioTransform";
 
-/**
- * Options for creating a new WebAudioPositioner.
- */
+/** @internal */
 export interface WebAudioPositionerOptions extends AudioPositionerOptions {}
 
-/**
- * Creates a new audio positioner.
- * @param parent - The parent node.
- * @param options - The options for creating the positioner.
- * @returns A promise that resolves with the created positioner.
- */
+/** @internal */
 export async function CreateAudioPositionerAsync(parent: AbstractAudioNode, options: Nullable<WebAudioPositionerOptions> = null): Promise<AudioPositioner> {
     return new WebAudioPositioner(parent, options);
 }
