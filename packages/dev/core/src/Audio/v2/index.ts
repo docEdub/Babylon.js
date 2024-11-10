9a380bd7a5a6b622912a6fc9fc0016b35888a9e8
Andy Fillebrown <andy@docEdub.com>
2024-11-10 01:50:33 -0500

    Get new audio classes working in UMD build

---
+++ packages/dev/core/src/Audio/v2/index.ts
@@ -0,0 +1,9 @@
+// TODO: What's the best way to expose the WebAudio factory functions without exposing the WebAudio classes?
+export * from "./webAudio/webAudioBus";
+export * from "./webAudio/webAudioEngine";
+export * from "./webAudio/webAudioMainBus";
+export * from "./webAudio/webAudioMainOutput";
+export * from "./webAudio/webAudioPositioner";
+export * from "./webAudio/webAudioSender";
+export * from "./webAudio/webAudioStaticSound";
+export * from "./webAudio/webAudioStreamingSound";
