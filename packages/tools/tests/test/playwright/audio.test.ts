bde94a0ca6d34e6e4d91037e4d4ab8642425c0d9
Andy Fillebrown <afillebrown@microsoft.com>
2024-11-11 15:34:14 -0500

    Failed attempt (but close) to get playwright audio test working with whisper.cpp
    Fails only because `SharedArrayBuffer` is not enabled.
    
    Tabled for later after discussing w/ Raanan. I'll get this working in my separate testing repo for now and we'll integrate it cleanly later.

--- packages/tools/tests/test/playwright/audio.test.ts
+++ packages/tools/tests/test/playwright/audio.test.ts
@@ -4,15 +4,17 @@ import * as path from "path";
 import { expect, Page, test } from "@playwright/test";
 import { getGlobalConfig } from "@tools/test-tools";
 import "babylonjs";
-import "chai";
 
 /* eslint-disable @typescript-eslint/naming-convention */
 declare global {
     interface Window {
         audioContext: AudioContext | null;
+        Whisper: any;
+        whisper: any;
     }
 
     const audioContext: AudioContext;
+    const Whisper: any;
 }
 
 const evaluatePlaywrightAudioTests = async (engineType = "webaudio", testFileName = "config", debug = false, debugWait = false, logToConsole = true, logToFile = false) => {
@@ -52,14 +54,23 @@ const evaluatePlaywrightAudioTests = async (engineType = "webaudio", testFileNam
 
     test.beforeAll(async ({ browser }) => {
         page = await browser.newPage();
-        await page.goto(getGlobalConfig({ root: config.root }).baseUrl + `/empty.html`, {
+        await page.goto(getGlobalConfig({ root: config.root }).baseUrl + `/empty-audio.html`, {
             // waitUntil: "load", // for chrome should be "networkidle0"
             timeout: 0,
         });
 
-        await page.waitForFunction(() => {
-            return window.BABYLON;
+        await page.evaluate(async () => {
+            console.log("");
+            const whisper = new Whisper();
+            await whisper.init();
+            window.whisper = whisper;
+            console.log("");
         });
+
+        await page.waitForFunction(() => {
+            return window.BABYLON && window.Whisper;
+        });
+
         page.setDefaultTimeout(0);
     });
 
@@ -92,6 +103,97 @@ const evaluatePlaywrightAudioTests = async (engineType = "webaudio", testFileNam
                 return engine;
             });
             expect(engine).toBeTruthy();
+        });
+    });
+    test.describe("R+D", () => {
+        test("can an AudioBuffer be returned?", async () => {
+            const audioArray = await page.evaluate(async () => {
+                async function soundEnded(sound: BABYLON.AbstractSound): Promise<void> {
+                    return new Promise<void>((resolve) => {
+                        if (audioContext instanceof OfflineAudioContext) {
+                            resolve();
+                        } else if (audioContext instanceof AudioContext || audioContext === undefined) {
+                            sound.onEndedObservable.addOnce(() => {
+                                resolve();
+                            });
+                        }
+                    });
+                }
+
+                const engine = (await BABYLON.CreateAudioEngineAsync({ audioContext: audioContext! })) as BABYLON.WebAudioEngine;
+
+                // Start audio capture.
+                const webAudioMainOutput = engine.mainOutput as BABYLON.WebAudioMainOutput;
+                const nodeToCapture = webAudioMainOutput.webAudioInputNode;
+                const recorderDestination = new MediaStreamAudioDestinationNode(audioContext);
+                const recorder = new MediaRecorder(recorderDestination.stream);
+                nodeToCapture.connect(recorderDestination);
+                recorder.start();
+
+                const sound = await BABYLON.CreateSoundAsync("", "https://amf-ms.github.io/AudioAssets/testing/3-count.mp3", engine, { autoplay: true });
+
+                await soundEnded(sound);
+
+                // Get captured audio.
+                let renderedBuffer: AudioBuffer;
+
+                await new Promise<void>((resolve) => {
+                    recorder.addEventListener(
+                        "dataavailable",
+                        async (event) => {
+                            const arrayBuffer = await event.data.arrayBuffer();
+                            const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+                            // Convert audio buffer to 16kHz sample rate required by whisper.cpp.
+                            const offlineAudioContext = new OfflineAudioContext(2, audioBuffer.duration * 16000, 16000);
+                            const source = new AudioBufferSourceNode(offlineAudioContext, { buffer: audioBuffer });
+                            source.connect(offlineAudioContext.destination);
+                            source.start();
+                            renderedBuffer = await offlineAudioContext.startRendering();
+
+                            recorderDestination.disconnect();
+
+                            resolve();
+                        },
+                        { once: true }
+                    );
+                    recorder.stop();
+                });
+
+                if (!(renderedBuffer! instanceof AudioBuffer)) {
+                    throw new Error("No buffer rendered.");
+                }
+
+                if (renderedBuffer.length === 0) {
+                    throw new Error("No audio data to transcribe.");
+                }
+
+                return renderedBuffer.getChannelData(0);
+            });
+
+            expect(audioArray.length).toBeGreaterThan(0);
+
+            // console.log("");
+            // const whisper = new Whisper();
+            // await whisper.init();
+            // console.log("");
+
+            // const result = await whisper.transcribe(audioArray);
+
+            // let sttOutput = await whisper.getText();
+            // console.log("raw sttOutput:", sttOutput);
+
+            // // Remove the trailing [BLANK_AUDIO] added by whisper.
+            // sttOutput = sttOutput.replace("[BLANK_AUDIO]", "");
+
+            // // Remove spaces.
+            // sttOutput = sttOutput.replace(/\s+/g, "");
+
+            // // Remove punctuation, hyphens and parenthesis added by whisper.
+            // sttOutput = sttOutput.replace(/(\,|\.|\-|\(|\))/g, "");
+            // sttOutput = sttOutput.toLowerCase();
+
+            // console.log("sttOutput:", sttOutput);
         });
     });
 };
