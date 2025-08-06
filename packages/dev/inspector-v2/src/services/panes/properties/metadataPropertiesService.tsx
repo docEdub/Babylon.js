54b60d7f339050d3f1e437cb8a5513265f4ff1a8
Andy Fillebrown <andy@docEdub.com>
2025-08-05 22:20:08 -0400

    wip on adding metadata properties

--- packages/dev/inspector-v2/src/services/panes/properties/metadataPropertiesService.tsx
+++ packages/dev/inspector-v2/src/services/panes/properties/metadataPropertiesService.tsx
@@ -1,20 +1,18 @@
 import type { IMetadataContainer } from "../../../components/properties/metadataProperties";
 import type { ServiceDefinition } from "../../../modularity/serviceDefinition";
-import type { ISettingsContext } from "../../settingsContext";
 import type { IPropertiesService } from "./propertiesService";
 
 import { MetadataProperties } from "../../../components/properties/metadataProperties";
-import { SettingsContextIdentity } from "../../settingsContext";
 import { PropertiesServiceIdentity } from "./propertiesService";
 
 function IsMetadataContainer(entity: unknown): entity is IMetadataContainer {
     return (entity as IMetadataContainer).metadata !== undefined;
 }
 
-export const MetadataPropertiesServiceDefinition: ServiceDefinition<[], [IPropertiesService, ISettingsContext]> = {
+export const MetadataPropertiesServiceDefinition: ServiceDefinition<[], [IPropertiesService]> = {
     friendlyName: "Metadata Properties",
-    consumes: [PropertiesServiceIdentity, SettingsContextIdentity],
-    factory: (propertiesService, settingsContent) => {
+    consumes: [PropertiesServiceIdentity],
+    factory: (propertiesService) => {
         const contentRegistration = propertiesService.addSectionContent({
             key: "Metadata Properties",
             // TransformNode and Bone don't share a common base class, but both have the same transform related properties.
@@ -22,7 +20,7 @@ export const MetadataPropertiesServiceDefinition: ServiceDefinition<[], [IProper
             content: [
                 {
                     section: "Metadata",
-                    component: ({ context }) => <MetadataProperties entity={context} settings={settingsContent} />,
+                    component: ({ context }) => <MetadataProperties entity={context} />,
                 },
             ],
         });
